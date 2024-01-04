31e0fdbbcfb792c0b310b8c3c1f6ab8eccfb7bf4
rizkifadilah12 <rf8740072@gmail.com>
2024-01-04 17:09:46 +0700

    tugas 2 di react

---
+++ src/components/content.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <main>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ab
+        placeat laboriosam magnam nemo necessitatibus, molestias voluptates sit.
+        Suscipit ad ab laboriosam aperiam fugit aliquid eum quo molestias
+        provident illo.
+      </p>
+    </main>
+  );
+};
+
+export default MainContent;
