1acd9eb963644f4515d61e2458d770e3891b7c31
rizkifadilah12 <rf8740072@gmail.com>
2024-01-09 18:38:46 +0700

    hooks dan functional component

--- src/components/PortalBerita.jsx
+++ src/components/PortalBerita.jsx
@@ -1,21 +1,16 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import NavbarBerita from "./Navbar";
-class PortalBerita extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      searchTerm: "",
-    };
-  }
 
-  componentDidMount() {
-    this.getNews();
-  }
+const PortalBerita = () => {
+  const [articles, setArticles] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  getNews = async () => {
-    const { searchTerm } = this.state;
+  useEffect(() => {
+    getNews();
+  }, [searchTerm]);
+
+  const getNews = async () => {
     const apiKey = "df7de5be60e244ec9a89518cf801c7ba";
     const apiUrl = "https://newsapi.org/v2/top-headlines?sources=bbc-news";
 
@@ -28,60 +23,54 @@ class PortalBerita extends Component {
       });
 
       const fetchedArticles = response.data.articles;
-      this.setState({ articles: fetchedArticles });
+      setArticles(fetchedArticles);
     } catch (error) {
       console.error("Error fetching news:", error);
     }
   };
 
-  handleSearch = (e) => {
-    this.setState({ searchTerm: e.target.value }, () => {
-      this.getNews();
-    });
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
   };
 
-  render() {
-    const { articles, searchTerm } = this.state;
-
-    return (
-      <div>
-        <NavbarBerita />
-        <br />
-        <input
-          type="text"
-          placeholder="Search News"
-          value={searchTerm}
-          onChange={this.handleSearch}
-        />
-        <h4>&nbsp;</h4>
-        <div className="row">
-          {articles.map((article) => (
-            <div key={article.url} className="col-md-4 mb-4">
-              <div className="card">
-                <img
-                  src={article.urlToImage}
-                  className="card-img-top"
-                  alt={article.title}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{article.title}</h5>
-                  <p className="card-text">{article.description}</p>
-                  <a
-                    href={article.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="btn btn-primary"
-                  >
-                    Read More
-                  </a>
-                </div>
+  return (
+    <div>
+      <NavbarBerita />
+      <br />
+      <input
+        type="text"
+        placeholder="Search News"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <h4>&nbsp;</h4>
+      <div className="row">
+        {articles.map((article) => (
+          <div key={article.url} className="col-md-4 mb-4">
+            <div className="card">
+              <img
+                src={article.urlToImage}
+                className="card-img-top"
+                alt={article.title}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{article.title}</h5>
+                <p className="card-text">{article.description}</p>
+                <a
+                  href={article.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn btn-primary"
+                >
+                  Read More
+                </a>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default PortalBerita;
